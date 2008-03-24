bd018891d579cffc65e5ae8ed470f278e97bd7dc
Pat Nakajima <patnakajima@gmail.com>
2008-03-24 14:01:39 -0400

    removed database clobber task, agnosticized start, stop and restart tasks

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -7,15 +7,15 @@ namespace :deploy do
   end
 
   task :restart do
-    run "mongrel_rails cluster::restart -C #{mongrel_conf}" 
+    send("#{app_server.to_s}.restart")
   end
   
   task :start do
-    run "mongrel_rails cluster::start -C #{mongrel_conf}" 
+    send("#{app_server.to_s}.start")
   end
   
   task :stop do
-    run "mongrel_rails cluster::stop -C #{mongrel_conf}" 
+    send("#{app_server.to_s}.stop")
   end
 
   desc "Shows tail of production log"
@@ -35,23 +35,4 @@ namespace :deploy do
       run "cp #{shared_path}/config/#{file} #{release_path}/config/"
     end
   end
-  
-  desc "Nuke and rebuild production db"
-  task :reset_db do
-    puts 'Are you sure? This action is unreversible. You will lose data.'
-    print 'Do you still wish to reset the db? [y/n] '
-    result = $stdin.gets.chomp
-    if result[0..0].match(/y/i)
-      puts 'clearing db...'
-      run "cd #{current_path} && RAILS_ENV='production' rake db:migrate VERSION=0"
-      puts 'rebuilding db...'
-      run "cd #{current_path} && RAILS_ENV='production' rake db:migrate"
-      puts 'loading admin...'
-      run "cd #{current_path} && RAILS_ENV='production' rake utils:load_admin"
-      puts 'all done!'
-    else
-      puts 'action canceled'
-    end
-  end
-
 end
