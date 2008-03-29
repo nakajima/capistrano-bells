aa936dd7cdf130273d62d8c7a7d0d0ef661b6006
Pat Nakajima <patnakajima@gmail.com>
2008-03-28 22:50:02 -0400

    removed unnecessary init.rb, updated install.rb with instructions

--- install.rb
+++ install.rb
@@ -1,1 +1,2 @@
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+puts "You've just installed Capistrano Bells."
+puts "Now run rake bells:generate_capfile to generate a new Capfile template and you'll be on your way."
