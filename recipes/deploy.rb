194247ae44cd2dcd3f0a78d600751a38f10d8249
Pat Nakajima <patnakajima@gmail.com>
2008-05-20 13:48:57 -0400

    started work on making bells a bit more framework agnostic

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -2,7 +2,7 @@ namespace :deploy do
   namespace :db do
     desc "Runs rake db:create on remote server"
     task :create do
-      run "cd #{current_path} && RAILS_ENV=#{mongrel_environment} rake db:create"
+      run "cd #{current_path} && #{app_framework}_ENV=#{mongrel_environment} rake db:create"
     end
   end
 
@@ -22,10 +22,15 @@ namespace :deploy do
   task :tail do
     stream "tail -f #{current_path}/log/#{app_environment}.log"
   end
+  
+  after 'deploy:setup', 'deploy:create_config_dir'
+  task :create_config_dir do
+    run "mkdir -p #{shared_path}/config"
+  end
 
   after "deploy:update_code", "deploy:copy_config_files"
   desc "Copy production database.yml to live app"
-  task :copy_config_files do
+  task :copy_config_files do    
     config_files.each do |file|
       run "cp #{shared_path}/config/#{file} #{release_path}/config/"
     end
